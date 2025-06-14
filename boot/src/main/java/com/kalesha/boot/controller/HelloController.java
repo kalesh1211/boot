5ec467edfa396ff9ddc6c572a1304c70418b2778
shaik vali <kaleshalocal54@gmail.com>
2025-06-14 10:13:33 +0530

    Initial commit of Spring Boot project structure

---
+++ boot/src/main/java/com/kalesha/boot/controller/HelloController.java
@@ -0,0 +1,48 @@
+package com.kalesha.boot.controller;
+
+import com.kalesha.boot.model.User;
+import com.kalesha.boot.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class HelloController {
+
+    private final UserService userService;
+
+    public HelloController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, Kalesha!";
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id).orElse(null);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User user) {
+        return userService.updateUser(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
+}
